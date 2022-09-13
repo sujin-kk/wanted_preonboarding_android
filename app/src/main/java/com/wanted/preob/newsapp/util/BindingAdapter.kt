9539d112ba6651c1914b2d6dfc24814265c7e7d4
sujin-kk <sjoo0002@gmail.com>
2022-09-13 16:04:43 +0900

    [feat] : detail news binding

--- app/src/main/java/com/wanted/preob/newsapp/util/BindingAdapter.kt
+++ app/src/main/java/com/wanted/preob/newsapp/util/BindingAdapter.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.wanted.preob.newsapp.R
 import java.text.SimpleDateFormat
 import java.util.*
@@ -25,5 +27,17 @@ fun ImageView.bindCategoryImage(resId: Int) {
     Glide.with(this)
         .load(resId)
         .into(this)
+}
+
+@BindingAdapter("bindDetailNewsImage")
+fun ImageView.bindDetailNewsImage(imgUrl: String?) {
+    imgUrl?.let {
+        Glide.with(this)
+            .load(imgUrl)
+            .error(R.drawable.ic_none_img)
+            .placeholder(R.drawable.ic_loading)
+            .transform(CenterCrop(), RoundedCorners(20))
+            .into(this)
+    }
 }
 
